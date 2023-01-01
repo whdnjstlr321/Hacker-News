2861c79b52a3a420ff641a80ccc7ce05769b447d
wonsik <whdnjstlr321@naver.com>
2023-01-01 19:53:55 +0900

    feat: print-hacker-news-detail
    1. 기존 타이틀의 태그를 <a>태그로 감싸게 변경
     - 타이틀을 누를 시 해당하는 id 값으로 hash 이동하게 변경
    2. hashchange 이벤트 적용
     - hash 변경 시 변경 된 해시 값으로 상세내용을 rest api로 요청하게 변경
     - rest api를 통해 받은 상세 내용의 타이틀을 출력하게 변경

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
-console.log(newsFeed);
+window.addEventListener('hashchange', () => {
+    const id = location.hash.substring(1);
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+    content.appendChild(title);
+    console.log(newsContent);
+})
 
 for (let i = 0; i < newsFeed.length; i++) {
     const li = document.createElement('li');
-    li.innerHTML = newsFeed[i].title;
+    const a = document.createElement('a');
+
+    a.href = `#${newsFeed[i].id}`;
+    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+    li.appendChild(a);
     ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
