95e6133f108099168568318a201978beb19cfafc
wonsik <whdnjstlr321@naver.com>
2023-01-01 21:33:11 +0900

    refactor: news feed 랜더링 관련 코드 함수화
    refactor: news detail 랜더링 관련 코드 함수화
    
    feature: news detail 뒤로가기(news feed로 이동) 기능 및 UI
    
    feature: route 기능 생성
    
    refactor: hash change 함수가 route 함수를 호출하게 변경

--- app.js
+++ app.js
@@ -10,33 +10,49 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
 
-window.addEventListener('hashchange', () => {
+    const newsList = [];
+    newsList.push('<ul>');
+    for (let i = 0; i < newsFeed.length; i++) {
+        newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
+    newsList.push('</ul>');
+
+    container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
     const id = location.hash.substring(1);
 
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
 
-    title.innerHTML = newsContent.title;
-    content.appendChild(title);
-    console.log(newsContent);
-})
+    container.innerHTML = `
+    <h1>${newsContent.title}</h1>
 
-for (let i = 0; i < newsFeed.length; i++) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
-    `
-
-    ul.appendChild(div.firstElementChild);
+    <div>
+        <a href="#">목록으로</a>
+    </div>
+    `;
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+    const routePath = location.hash;
+    if(routePath === '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
