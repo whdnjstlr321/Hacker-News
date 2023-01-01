b195ac02dbe00dd7a384561623d4fc4d1ef97e8c
wonsik <whdnjstlr321@naver.com>
2023-01-01 20:56:55 +0900

    refactor: rest api 호출 기능을 함수화
    refactor: dom element를 생성하는 부분 가시화

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+    return JSON.parse(ajax.response);
+}
 
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', () => {
     const id = location.hash.substring(1);
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
 
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
 
     title.innerHTML = newsContent.title;
@@ -24,14 +25,17 @@ window.addEventListener('hashchange', () => {
 })
 
 for (let i = 0; i < newsFeed.length; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    const div = document.createElement('div');
 
-    a.href = `#${newsFeed[i].id}`;
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    div.innerHTML = `
+    <li>
+        <a href="#${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+    </li>
+    `
 
-    li.appendChild(a);
-    ul.appendChild(li);
+    ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
