2182980f712fdc6ae0f22ce83ba5b422031afd42
wonsik <whdnjstlr321@naver.com>
2023-01-02 22:42:50 +0900

    feat: 페이징 기능 구현

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     ajax.open('GET', url, false);
@@ -15,22 +18,27 @@ function newsFeed() {
 
     const newsList = [];
     newsList.push('<ul>');
-    for (let i = 0; i < newsFeed.length; i++) {
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
         <li>
-            <a href="#${newsFeed[i].id}">
+            <a href="#/show/${newsFeed[i].id}">
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
         `);
     }
     newsList.push('</ul>');
-
+    newsList.push(`
+    <div>
+        <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+        <a href="#/page/${store.currentPage < Math.ceil(newsFeed.length / 10) ? store.currentPage + 1 : Math.ceil(newsFeed.length / 10)}">다음 페이지</a>
+    </div>
+    `);
     container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
 
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
@@ -39,7 +47,7 @@ function newsDetail() {
     <h1>${newsContent.title}</h1>
 
     <div>
-        <a href="#">목록으로</a>
+        <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
     `;
 }
@@ -47,6 +55,9 @@ function newsDetail() {
 function router() {
     const routePath = location.hash;
     if(routePath === '') {
+        newsFeed();
+    } else if(routePath.indexOf('#/page/') !== -1) {
+        store.currentPage = Number(routePath.substring(7));
         newsFeed();
     } else {
         newsDetail();
